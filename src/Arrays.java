2069668ac54eea573536b6a43a13f98343c4169c
AidanSugrue28 <aidan_sugrue28@milton.edu>
2026-02-25 20:10:36 -0500

    added a print for avg, largest, sum, and smallest number in the array

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,7 +2,6 @@ public class Arrays {
 
     public int[] nums;
 
-
     public static void main(String[] args){
         System.out.println("welcome to the awesome world of arrays");
         new Arrays();
@@ -12,19 +11,21 @@ public class Arrays {
         nums = new int[10];
         for (int y = 0; y < nums.length; y++){
             nums[y] = (int)(Math.random()*100)+1;
-            }
-
+        }
 
         System.out.println(nums[0]);
 
         displayArrays();
-
         sumArray();
+        averageNum();
+        maxNum();
+        minNum();
     }
+
     public void displayArrays(){
-       for(int x = 0; x < nums.length; x++){
-           System.out.println(nums[x]);
-       }
+        for(int x = 0; x < nums.length; x++){
+            System.out.println(nums[x]);
+        }
     }
 
     public void sumArray(){
@@ -35,6 +36,43 @@ public class Arrays {
         }
 
         System.out.println("Sum of array: " + sum);
-        }
-        
     }
+
+    // 1. Average method
+    public void averageNum(){
+        int sum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+
+        double average = (double) sum / nums.length;
+        System.out.println("Average of array: " + average);
+    }
+
+    // 2. Max number method
+    public void maxNum(){
+        int max = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+
+        System.out.println("Largest number in array: " + max);
+    }
+
+    // 3. Min number method
+    public void minNum(){
+        int min = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] < min){
+                min = nums[i];
+            }
+        }
+
+        System.out.println("Smallest number in array: " + min);
+    }
+}
